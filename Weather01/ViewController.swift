e80bbc1ba6939684a64ac43bab7a4d4789182690
vjosullivan <vjosullivan@gmail.com>
2017-01-07 17:21:24 +0000

    Initial Commit

---
+++ Weather01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather01
+//
+//  Created by Vincent O'Sullivan on 07/01/2017.
+//  Copyright © 2017 Rose Cottage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
